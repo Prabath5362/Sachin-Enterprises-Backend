faf4fa8a93aae80448dad9d0bfe48f8f5449130e
Prabath5362 <nethminaprabath@gmail.com>
2025-09-20 13:22:12 +0530

    backend fix

--- controller/userController.js
+++ controller/userController.js
@@ -63,7 +63,7 @@ export async function loginUser(req, res) {
 
     } catch (e) {
         res.status(500).json({
-            message: "Login error "+ e.message,
+            message: "Login error: "+ e.message,
         });
     }
 }
